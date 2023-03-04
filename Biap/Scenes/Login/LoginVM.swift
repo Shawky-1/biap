b646b995146c0bd6db3c7851db74d7f580c2908d
Ahmed Shawky <ahmed.shawky.alabiad@gmail.com>
2023-03-04 16:15:38 +0200

    LoginScreen

---
+++ Biap/Scenes/Login/LoginVM.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVM.swift
+//  Biap
+//
+//  Created by Ahmed Shawky on 04/03/2023.
+//
+
+import Foundation
+
+class LoginVM: ViewModel {
+    func viewDidLoad() {
+        
+    }
+    
+    
+}
